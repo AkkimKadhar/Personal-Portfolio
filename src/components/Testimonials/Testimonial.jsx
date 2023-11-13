e92b5a9a1e03356b2c473abcafce3cdd5eb59493
AkkimKadhar <akkimkadhar1@gmail.com>
2023-11-13 12:02:20 +0530

    new

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -30,7 +30,7 @@ const Testimonial = () => {
     {
       img: profilePic4,
       review:
-        "Simple enough to learn quickly and get around easily, complicated enough to get really organized and track a lot of info. Love the visually simple design. And love the collaboration possible with clients ishu",
+        "ishu Simple enough to learn quickly and get around easily, complicated enough to get really organized and track a lot of info. Love the visually simple design. And love the collaboration possible with clients",
     },
   ];
 
